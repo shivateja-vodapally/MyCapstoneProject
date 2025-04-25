fa4bc9878e46cb25a3af1658cba229a58c9d879c
shivateja-vodapally <139704342+shivateja-vodapally@users.noreply.github.com>
2025-04-25 21:16:26 +0530

    New project added to repo

---
+++ ProductCatalogServiceProxy/src/test/java/ProductCatalogServiceProxy/ProductCatalogServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package ProductCatalogServiceProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
